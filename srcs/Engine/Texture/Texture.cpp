05ae331646cbc8e07b06ce0ca78e5292e4249125
averdon <anthonyverdon10@gmail.com>
2025-04-01 13:04:55 +0200

    update texture: delete ppm, clean loading via file or buffer
    todo: think about what we should do with path attribute in case its not set, add back skybox loading later

--- srcs/Engine/Texture/Texture.cpp
+++ srcs/Engine/Texture/Texture.cpp
@@ -1,36 +1,35 @@
-#include "Engine/Texture/Texture.hpp"
-#include <glad/glad.h>
-#include "Toolbox.hpp"
+#include "Texture.hpp"
 #define STB_IMAGE_IMPLEMENTATION
+#include <filesystem>
+#include <glad/glad.h>
+#include <iostream>
 #include <stb/stb_image.h>
+#include <vector>
 
-bool Texture::textureInit = false;
-
-Texture::Texture(const std::string &texturePath)
+Texture::Texture(const std::string &path)
 {
-    if (!textureInit)
-        initTexParameter();
-
-    ID = 0;
-    loadImage(texturePath);
-
-    glGenTextures(1, &ID);
-    glBindTexture(GL_TEXTURE_2D, ID);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, &data[0]);
-    glGenerateMipmap(GL_TEXTURE_2D);
+    this->path = path; //@todo check path
+    int nrChannels;
+    stbi_set_flip_vertically_on_load(false);
+    unsigned char *data = stbi_load(path.c_str(), &width, &height, &nrChannels, 0);
+    GenerateTexture(data, width, height, nrChannels);
 }
 
-Texture::Texture(const unsigned char *buffer, size_t length)
+Texture::Texture(const unsigned char* buffer, size_t length)
 {
-    if (!textureInit)
-        initTexParameter();
-
-    int width;
-    int height;
+    path = ""; //@todo, what should we do with this param in that case ?
     int nrChannels;
     stbi_set_flip_vertically_on_load(false);
     unsigned char *data = stbi_load_from_memory(buffer, length, &width, &height, &nrChannels, 0);
+    GenerateTexture(data, width, height, nrChannels);
+}
 
+Texture::~Texture()
+{
+}
+
+void Texture::GenerateTexture(unsigned char *data, int width, int height, int nrChannels)
+{
     unsigned int format = 0;
     if (nrChannels == 3)
         format = GL_RGB;
@@ -43,114 +42,9 @@ Texture::Texture(const unsigned char *buffer, size_t length)
     glGenerateMipmap(GL_TEXTURE_2D);
 
     stbi_image_free(data);
-}
 
-Texture::Texture(const Texture &copy)
-{
-    *this = copy;
-}
-
-Texture &Texture::operator=(const Texture &copy)
-{
-    if (&copy != this)
-    {
-        this->ID = copy.getID();
-    }
-    return (*this);
-}
-
-Texture::~Texture()
-{
-}
-
-void Texture::checkHeader(const std::string &line, unsigned int nbLine)
-{
-    std::vector<std::string> words;
-
-    switch (nbLine)
-    {
-    case 0:
-        if (line != "P3")
-            throw(Exception("CHECK_HEADER", "INVALID_LINE", line, nbLine + 1));
-        break;
-    case 1: {
-        words = Toolbox::splitLine(line, " ");
-        if (words.size() != 2)
-            throw(Exception("CHECK_HEADER", "INVALID_LINE", line, nbLine + 1));
-
-        width = std::stoi(words[0]);
-        height = std::stoi(words[1]);
-        data = std::make_unique<unsigned char[]>(width * height * 3);
-        break;
-    }
-    case 2: {
-        words = Toolbox::splitLine(line, " ");
-        if (words.size() != 1)
-            throw(Exception("CHECK_HEADER", "INVALID_LINE", line, nbLine + 1));
-
-        valueMax = std::stoi(words[0]);
-        break;
-    }
-    default:
-        break;
-    }
-}
-
-void Texture::loadImage(const std::string &texturePath)
-{
-    std::string line;
-    std::vector<std::string> words;
-    unsigned int pixel;
-    unsigned int nbLine;
-
-    pixel = 0;
-    nbLine = 0;
-    std::string file = Toolbox::readFile(texturePath);
-    std::vector<std::string> lines = Toolbox::splitLine(file, "\n");
-    for (size_t i = 0; i < lines.size(); i++)
-    {
-        line = lines[i].substr(0, lines[i].find("#"));
-        if (line.length() == 0)
-            continue;
-        else if (nbLine < 3)
-            checkHeader(line, nbLine);
-        else
-        {
-            words = Toolbox::splitLine(line, " ");
-            for (size_t j = 0; j < words.size(); j++)
-            {
-                data[pixel] = std::stoi(words[j]);
-                pixel++;
-            }
-        }
-        nbLine++;
-    }
-}
-
-void Texture::initTexParameter()
-{
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    textureInit = true;
-}
-
-unsigned int Texture::getID() const
-{
-    return (ID);
-}
-
-Texture::Exception::Exception(const std::string &functionName, const std::string &errorMessage, const std::string &line,
-                              unsigned int lineIndex)
-{
-    this->errorMessage = "TEXTURE::" + functionName + "::" + errorMessage;
-    this->errorMessage += "\n|\n| " + std::to_string(lineIndex) + ": " + line + "\n|";
-}
-
-const char *Texture::Exception::what(void) const throw()
-{
-    return (errorMessage.c_str());
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 }
