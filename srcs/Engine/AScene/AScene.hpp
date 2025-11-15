91030f7116a3f0395a186f52ed246690a8860cc9
Anthony Verdon <anthonyverdon10@gmail.com>
2025-11-15 22:49:26 +0100

    add AScene.hpp

---
+++ srcs/Engine/AScene/AScene.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+class AScene
+{
+  public:
+    AScene() {}
+    virtual ~AScene() {}
+
+    virtual void Load(){};
+    virtual void Run(){};
+    virtual void Quit(){};
+
+    virtual int GetID() = 0;
+};
