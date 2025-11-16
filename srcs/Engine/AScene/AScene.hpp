152e403e5c48a90c0285246ed4e64b126988f5b5
Anthony Verdon <anthonyverdon10@gmail.com>
2025-11-16 15:35:37 +0100

    change return value of AScene::Run to unique ptr of AScene
    its to create a better state machine, with each scene returning a possible scene to switch on

--- srcs/Engine/AScene/AScene.hpp
+++ srcs/Engine/AScene/AScene.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <memory>
+
 class AScene
 {
   public:
@@ -7,7 +9,7 @@ class AScene
     virtual ~AScene() {}
 
     virtual void Load(){};
-    virtual void Run(){};
+    virtual std::unique_ptr<AScene> Run() { return NULL; };
     virtual void Quit(){};
 
     virtual int GetID() = 0;
